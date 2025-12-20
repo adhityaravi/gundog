8845b61bc29a42785cea32ad54ba619209d75723
Adhitya Ravi <adhitya.ravi@canonical.com>
2025-12-20 21:19:21 +0100

    chore: drop dead tui legacy code

--- packages/client/src/gundog_client/_tui/_modals/__init__.py
+++ packages/client/src/gundog_client/_tui/_modals/__init__.py
@@ -1,13 +1,7 @@
 """TUI modals for gundog."""
 
-from gundog_client._tui._modals._help import HelpModal
-from gundog_client._tui._modals._index import IndexModal
 from gundog_client._tui._modals._input import InputModal
-from gundog_client._tui._modals._preview import PreviewModal
 
 __all__ = [
-    "HelpModal",
-    "IndexModal",
     "InputModal",
-    "PreviewModal",
 ]
